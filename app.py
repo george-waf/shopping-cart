102661d5bd9e2ab5380ec3ef0a075ca634e04f69
George Wafula <georgewafulan@gmail.com>
2017-09-21 18:07:53 +0300

    updated the login functions

--- app.py
+++ app.py
@@ -1,22 +1,54 @@
-from flask import Flask, render_template
+from models.model import *
+from flask import Flask, render_template,redirect, request,url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        for user in users:
+            if username == user.username:
+                if password == user.password:
+                    
+                    return redirect(url_for('dashboard'))
+                else:
+                    if username or password:
+                        return redirect(url_for('/'))
+    return render_template("index.html")
 
-@app.route('/register')
+   
+
+@app.route('/register',methods=['POST', 'GET'])
 def register():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        user = User(firstname,lastname,username,email,password)
+        users.append(user)     
+        return redirect(url_for('index'))
     return render_template('register.html')
 
-@app.route('/dashboard')
+@app.route('/dashboard' ,methods=['POST','GET'])
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/newshoppinglist')
+@app.route('/newshoppinglist' ,methods=['POST','GET'])
 def newshoppinglist():
-    return render_template('newshoppinglist.html')
+    if request.method == 'POST':
+        shoppinglist = request.form['shoppinglist']
+        description = request.form['description']
+        items = request.form['items']
+        status = request.form['status']
+        dateadded = request.form['dateadded']
+        shoppinglist = Shoppinglist(shoppinglist,description,items,status,dateadded)
+        shopping_list.append(shoppinglist)
+        return render_template("fullList.html", shopping_list = shopping_list)
+    return render_template("newshoppinglist.html")
 
 @app.route('/fullList')
 def fullList():
@@ -28,7 +60,7 @@ def editlist():
 
 @app.route('/deletelist')
 def deletelist():
-    return render_template('deletelist.html')      
+    return render_template('deletelist.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
