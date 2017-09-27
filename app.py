f12a708ea24693d6c0fdb35d344fa94218648446
George Wafula <georgewafulan@gmail.com>
2017-09-27 12:41:13 +0300

    updated sessions

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
 from models.model import *
-from flask import Flask, render_template,redirect, request,url_for
+from flask import Flask, render_template, redirect, request, url_for, session
+
+
+shopping_list = []
 
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
-@app.route('/',methods=['POST', 'GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+
         for user in users:
             if username == user.username:
                 if password == user.password:
-                    
-                    return redirect(url_for('dashboard'))
-                else:
-                    if username or password:
-                        return redirect(url_for('/'))
+                    session['logged_in'] = True
+                    session['id'] = username
+
+                return redirect(url_for('dashboard'))
+            else:
+                if username or password:
+                    return redirect(url_for('/'))
     return render_template("index.html")
 
-   
 
-@app.route('/register',methods=['POST', 'GET'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         firstname = request.form['firstname']
@@ -28,39 +36,61 @@ def register():
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-        user = User(firstname,lastname,username,email,password)
-        users.append(user)     
+        user = User(firstname, lastname, username, email, password)
+        users.append(user)
         return redirect(url_for('index'))
     return render_template('register.html')
 
-@app.route('/dashboard' ,methods=['POST','GET'])
-def dashboard():
-    return render_template('dashboard.html')
 
-@app.route('/newshoppinglist' ,methods=['POST','GET'])
+@app.route('/dashboard', methods=['POST', 'GET'])
+def dashboard():
+    return render_template('dashboard.html', list_items=shopping_list)
+
+
+@app.route('/newshoppinglist', methods=['POST', 'GET'])
 def newshoppinglist():
+
     if request.method == 'POST':
+
+        #listid = request.form['listid']
         shoppinglist = request.form['shoppinglist']
         description = request.form['description']
         items = request.form['items']
         status = request.form['status']
         dateadded = request.form['dateadded']
-        shoppinglist = Shoppinglist(shoppinglist,description,items,status,dateadded)
-        shopping_list.append(shoppinglist)
-        return render_template("fullList.html", shopping_list = shopping_list)
+        slist = Shoppinglist(shoppinglist, description,
+                             items, status, dateadded)
+        shopping_list.append(slist)
+        return render_template("fullList.html", list_items=shopping_list)
     return render_template("newshoppinglist.html")
+
 
 @app.route('/fullList')
 def fullList():
-    return render_template('fullList.html')    
 
-@app.route('/editlist')
+    return render_template('fullList.html', list_items=shopping_list)
+
+
+@app.route('/editlist', methods=['POST', 'GET'])
 def editlist():
-    return render_template('editlist.html')
+    if request.method == 'GET':
+        #listid = request.form['listid']
+        shoppinglist = request.form['shoppinglist']
+        description = request.form['description']
+        items = request.form['items']
+        status = request.form['status']
+        dateadded = request.form['dateadded']
+        slist = Shoppinglist(shoppinglist, description,
+                             items, status, dateadded)
+        shopping_list.append(slist)
+        return render_template("fullList.html", list_items=shopping_list)
+    return render_template("editlist.html")
 
-@app.route('/deletelist')
-def deletelist():
-    return render_template('deletelist.html')
+
+@app.route('/logout')
+def logout():
+   # session.pop('logged_in', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
