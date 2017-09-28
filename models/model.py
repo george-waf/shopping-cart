6bb614854b10b5dd118cc0be9746ba3803447c5e
George Wafula <georgewafulan@gmail.com>
2017-09-28 13:17:09 +0300

    update datetime function

--- models/model.py
+++ models/model.py
@@ -1,14 +1,13 @@
 users = []
 shopping_list = []
 
-
 from datetime import datetime
-
-
+ 
 class User(object):
 
     def __init__(self, firstname, lastname, username, email, password):
 
+        self.id = id(self)
         self.firstname = firstname
         self.lastname = lastname
         self.username = username
@@ -19,14 +18,10 @@ class User(object):
 class Shoppinglist(object):
 
     def __init__(self, shoppinglist, description, items, status='new', dateadded='datetime.now()'):
-        #self.listid = random.randrange(1, 1000, 1)
+        self.listid = id(self)
         self.shoppinglist = shoppinglist
         self.description = description
         self.items = items
         self.status = 'new'
         self.dateadded = datetime.now().strftime('%Y-%m-%d')
 
-        shopping_list.append(self)
-
-    def delete_list(self, listid):
-        shopping_list.remove(self)
